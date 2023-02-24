4a3420ac4fc50e21a057e14f8f3cc879fde355cc
Andrew <uazupyandex@gmail.com>
2023-02-24 18:02:53 +0800

    Домашняя работа 17.7 сделана

---
+++ Модуль 17 (Основы python)/17.7 Homework.py
@@ -0,0 +1,8 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("Введите сумму вклада: "))
+deposit = []
+for v in per_cent.values():
+    value = money/100*v
+    deposit.append(round(value,2))
+print (deposit)
+print ("Максимальная сумма, которую вы можете заработать -",max(deposit))
